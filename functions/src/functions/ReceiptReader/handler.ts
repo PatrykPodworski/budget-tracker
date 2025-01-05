84d839358c2f553242880f7c4e842ba77fc0e9cf
Patryk Podworski <patryk.podworski@gmail.com>
2025-01-05 10:29:03 +0100

    parse document to extract better raw data

--- functions/src/functions/ReceiptReader/handler.ts
+++ functions/src/functions/ReceiptReader/handler.ts
@@ -3,13 +3,16 @@ import {
   DocumentAnalysisClient,
 } from "@azure/ai-form-recognizer";
 import { InvocationContext, StorageBlobHandler } from "@azure/functions";
-import { PrebuiltReceiptModel } from "../../prebuilt/prebuilt-receipt";
+import {
+  PrebuiltReceiptDocument,
+  PrebuiltReceiptModel,
+} from "../../prebuilt/prebuilt-receipt";
 import { config } from "../../config";
 import { registerLogger } from "../../utils/logger/registerLogger";
 import { getDefaultChannels } from "../../utils/logger/getDefaultChannels";
 import { ReceiptRawData } from "../../models/ReceiptRawData";
 
-const handler: StorageBlobHandler = async (blob, context) => {
+export const handler: StorageBlobHandler = async (blob, context) => {
   const { addChannels, log } = registerLogger();
 
   addChannels(getDefaultChannels(context, "Receipt Reader"));
@@ -28,7 +31,6 @@ const handler: StorageBlobHandler = async (blob, context) => {
   const poller = await client.beginAnalyzeDocument(PrebuiltReceiptModel, blob);
   const {
     documents: [document],
-    content,
   } = await poller.pollUntilDone();
   await log("Document analyzed");
 
@@ -37,7 +39,7 @@ const handler: StorageBlobHandler = async (blob, context) => {
     return;
   }
   const imageFileName = getBlobName(context.triggerMetadata);
-  const data = mapToReceiptData(content, imageFileName);
+  const data = mapToReceiptData(document.fields, imageFileName);
 
   return data;
 };
@@ -54,7 +56,11 @@ const getBlobName = (triggerMetadata: InvocationContext["triggerMetadata"]) => {
   return triggerMetadata.blobTrigger;
 };
 
-const mapToReceiptData = (content: string, imageFileName: string) => {
+type Fields = PrebuiltReceiptDocument["fields"];
+
+const mapToReceiptData = (fields: Fields, imageFileName: string) => {
+  const content = extractContentFromDocument(fields);
+
   const data: ReceiptRawData = {
     id: crypto.randomUUID(),
     userId: config.TEMP_USER_ID,
@@ -65,4 +71,35 @@ const mapToReceiptData = (content: string, imageFileName: string) => {
   return data;
 };
 
-export { handler };
+const extractContentFromDocument = (fields: Fields) => {
+  const items =
+    fields.items?.values.map((x) => x.content).filter((x) => x !== undefined) ??
+    [];
+
+  const { transactionDate, transactionTime } =
+    extractTransactionDateTime(fields);
+
+  const content = {
+    merchantName: fields.merchantName?.content,
+    total: fields.total?.content,
+    transactionDate,
+    transactionTime,
+    items,
+  };
+
+  return JSON.stringify(content);
+};
+
+const extractTransactionDateTime = (fields: Fields) => {
+  if (!("transactionDate" in fields)) {
+    return {
+      transactionDate: undefined,
+      transactionTime: undefined,
+    };
+  }
+
+  return {
+    transactionDate: fields.transactionDate?.content,
+    transactionTime: fields.transactionTime?.content,
+  };
+};
--- functions/src/models/ReceiptRawData.ts
+++ functions/src/models/ReceiptRawData.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
 
-// TODO: P0 include items because content might be out of order
 export const receiptRawDataSchema = z.object({
   id: z.string().uuid(),
   userId: z.string().uuid(),
