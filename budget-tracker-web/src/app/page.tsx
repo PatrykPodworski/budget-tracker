42b07fe2ddca3a4182b68b24338b8bc068f368ec
Patryk Podworski <patryk.podworski@gmail.com>
2025-01-09 20:29:37 +0100

    Web read enriched data (#8)
    * query cosmos db container
    
    * use zod to parse envs
    
    * show basic receipt data

--- budget-tracker-web/src/app/page.tsx
+++ budget-tracker-web/src/app/page.tsx
@@ -1,8 +1,41 @@
-export default function Home() {
+import { CosmosClient } from "@azure/cosmos";
+import { env } from "@/env";
+import { enrichedReceiptDataSchema } from "@/models/enriched-receipt-data-schema";
+import { ReceiptsList } from "./receipts-list";
+
+// TODO: P1 Move the queryCosmosDb function to lib
+// TODO: P1 Show data like the discord bot
+// TODO: P2 Allow to update categories
+const Home = async () => {
+  const receipts = await queryCosmosDb();
+
   return (
     <main className="m-auto">
-      <h1>Budget Tracker</h1>
-      <p>Track your expenses with ease</p>
+      <div className="flex flex-col gap-4 max-w-prose">
+        <ReceiptsList receipts={receipts} />
+      </div>
     </main>
   );
-}
+};
+
+const queryCosmosDb = async () => {
+  const client = new CosmosClient({
+    endpoint: env.COSMOS_ENDPOINT,
+    key: env.COSMOS_KEY,
+  });
+  const container = client
+    .database(env.COSMOS_DATABASE)
+    .container(env.COSMOS_CONTAINER);
+
+  const data = await container.items
+    .query<unknown>("SELECT * FROM c ORDER BY c._ts DESC OFFSET 0 LIMIT 10")
+    .fetchAll();
+
+  const parsed = data.resources.map((item) =>
+    enrichedReceiptDataSchema.parse(item)
+  );
+
+  return parsed;
+};
+
+export default Home;
---
+++ budget-tracker-web/src/models/enriched-item-schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+// TODO: P2 Use monorepo and share this schema with other projects
+export const enrichedItemSchema = z.object({
+  originalName: z.string().min(1),
+  name: z.string().min(1),
+  category: z.string().min(1),
+  unitPrice: z.number().nonnegative(),
+  quantity: z.number().nonnegative(),
+  totalPrice: z.number().nonnegative(),
+  discount: z.number().nonnegative(),
+});
+
+export type EnrichedItem = z.infer<typeof enrichedItemSchema>;
