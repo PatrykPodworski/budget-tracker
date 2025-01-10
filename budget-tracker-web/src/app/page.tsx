e44091ceb2a3159181a81db796780d2792e71b8a
Patryk Podworski <patryk.podworski@gmail.com>
2025-01-10 19:28:34 +0100

    move cosmos code to lib

--- budget-tracker-web/src/app/page.tsx
+++ budget-tracker-web/src/app/page.tsx
@@ -1,13 +1,10 @@
-import { CosmosClient } from "@azure/cosmos";
-import { env } from "@/env";
-import { enrichedReceiptDataSchema } from "@/models/enriched-receipt-data-schema";
+import { getLatestReceipts } from "@/lib/receipt-data/get-latest-receipts";
 import { ReceiptsList } from "./receipts-list";
 
-// TODO: P1 Move the queryCosmosDb function to lib
 // TODO: P1 Show data like the discord bot
 // TODO: P2 Allow to update categories
 const Home = async () => {
-  const receipts = await queryCosmosDb();
+  const receipts = await getLatestReceipts();
 
   return (
     <main className="m-auto">
@@ -16,26 +13,6 @@ const Home = async () => {
       </div>
     </main>
   );
-};
-
-const queryCosmosDb = async () => {
-  const client = new CosmosClient({
-    endpoint: env.COSMOS_ENDPOINT,
-    key: env.COSMOS_KEY,
-  });
-  const container = client
-    .database(env.COSMOS_DATABASE)
-    .container(env.COSMOS_CONTAINER);
-
-  const data = await container.items
-    .query<unknown>("SELECT * FROM c ORDER BY c._ts DESC OFFSET 0 LIMIT 10")
-    .fetchAll();
-
-  const parsed = data.resources.map((item) =>
-    enrichedReceiptDataSchema.parse(item)
-  );
-
-  return parsed;
 };
 
 export default Home;
--- budget-tracker-web/src/app/receipts/[id]/page.tsx
+++ budget-tracker-web/src/app/receipts/[id]/page.tsx
@@ -1,6 +1,4 @@
-import { env } from "@/env";
-import { enrichedReceiptDataSchema } from "@/models/enriched-receipt-data-schema";
-import { CosmosClient } from "@azure/cosmos";
+import { getReceiptData } from "@/lib/receipt-data/get-receipt-data";
 
 const ReceiptPage = async ({ params }: ReceiptPageProps) => {
   const { id } = await params;
@@ -18,29 +16,6 @@ type ReceiptPageProps = {
   params: Promise<{
     id: string;
   }>;
-};
-
-const getReceiptData = async (id: string) => {
-  const client = new CosmosClient({
-    endpoint: env.COSMOS_ENDPOINT,
-    key: env.COSMOS_KEY,
-  });
-  const container = client
-    .database(env.COSMOS_DATABASE)
-    .container(env.COSMOS_CONTAINER);
-
-  const data = await container.items
-    .query<unknown>({
-      query: "SELECT * FROM c WHERE c.id = @id",
-      parameters: [{ name: "@id", value: id }],
-    })
-    .fetchNext();
-
-  const parsed = data.resources.map((item) =>
-    enrichedReceiptDataSchema.parse(item)
-  );
-
-  return parsed;
 };
 
 export default ReceiptPage;
