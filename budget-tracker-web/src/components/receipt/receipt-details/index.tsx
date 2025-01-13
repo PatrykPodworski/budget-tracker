6cbc9f81ffacbf7f751b7afbd34e90d1900a7c69
Patryk Podworski <patryk.podworski@gmail.com>
2025-01-13 22:56:36 +0100

    persist updated category in cosmos (#11)

--- budget-tracker-web/src/components/receipt/receipt-details/index.tsx
+++ budget-tracker-web/src/components/receipt/receipt-details/index.tsx
@@ -1,20 +1,39 @@
 "use client";
+import { useState } from "react";
 import { ExcelOutput } from "@/components/receipt/receipt-details/excel-output";
 import { Receipt } from "@/components/receipt/receipt-details/receipt";
+import { EnrichedItem } from "@/models/enriched-item-schema";
 import { EnrichedReceiptData } from "@/models/enriched-receipt-data-schema";
-import { useState } from "react";
+import { updateReceiptItem } from "@/lib/receipt-data/update-receipt-item";
 
+// TODO: P1 Add loading state
+// TODO: P1 Update item name
+// TODO: P2 Error handling on update
 export const ReceiptDetails = ({
   receipt: initialReceiptData,
 }: ReceiptDetailsProps) => {
   const [receipt, setReceipt] = useState(initialReceiptData);
-  const handleReceiptChange = (newReceipt: EnrichedReceiptData) => {
-    setReceipt(newReceipt);
+
+  const handleReceiptItemChange = async (
+    newItem: EnrichedItem,
+    index: number
+  ) => {
+    const updatedReceipt = await updateReceiptItem(
+      receipt.id,
+      receipt.userId,
+      newItem,
+      index
+    );
+
+    setReceipt(updatedReceipt);
   };
 
   return (
     <div>
-      <Receipt receipt={receipt} onReceiptChange={handleReceiptChange} />
+      <Receipt
+        receipt={receipt}
+        onReceiptItemChange={handleReceiptItemChange}
+      />
       <ExcelOutput items={receipt.items} />
     </div>
   );
