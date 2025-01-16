04eb005a16c492a5e2dff138668e4cc96750e779
Patryk Podworski <patryk.podworski@gmail.com>
2025-01-16 19:32:02 +0100

    Order by transaction date

--- budget-tracker-web/src/lib/receipt-data/get-latest-receipts.tsx
+++ budget-tracker-web/src/lib/receipt-data/get-latest-receipts.tsx
@@ -1,11 +1,14 @@
 import { enrichedReceiptDataSchema } from "@/models/enriched-receipt-data-schema";
 import { getReceiptContainer } from "./common/get-receipt-container";
 
+// TODO: P2 Add pagination
 export const getLatestReceipts = async () => {
   const container = getReceiptContainer();
 
   const data = await container.items
-    .query<unknown>("SELECT * FROM c ORDER BY c._ts DESC OFFSET 0 LIMIT 10")
+    .query<unknown>(
+      "SELECT * FROM c ORDER BY c.transactionDate DESC OFFSET 0 LIMIT 10"
+    )
     .fetchAll();
 
   const parsed = data.resources.map((item) =>
