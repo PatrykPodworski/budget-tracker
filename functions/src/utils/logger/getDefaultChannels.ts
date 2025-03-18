c1e1a230dc5743118b2a5aab6949e8d097265133
Patryk Podworski <patryk.podworski@gmail.com>
2025-03-18 22:49:00 +0100

    Remove discord ref from functions

--- functions/src/utils/logger/getDefaultChannels.ts
+++ functions/src/utils/logger/getDefaultChannels.ts
@@ -1,5 +1,4 @@
 import { InvocationContext } from "@azure/functions";
-import { getWebhookClient } from "../getWebhookClient";
 import { Logger } from "./registerLogger";
 
 export const getDefaultChannels = (
@@ -7,41 +6,12 @@ export const getDefaultChannels = (
   name: string
 ) => {
   const contextLogger = getContextLogger(context, name);
-  const webhookLogger = getWebhookLogger(name);
-
-  return [contextLogger, webhookLogger];
+  return [contextLogger];
 };
 
 const getContextLogger = (context: InvocationContext, name: string): Logger => {
   return async ({ message, level }) => {
     const messageWithName = `[${name}]: ${message}`;
     return Promise.resolve(context[level ?? "log"](messageWithName));
-  };
-};
-
-const getWebhookLogger = (name: string): Logger => {
-  const logClient = getWebhookClient();
-
-  return async ({ message, level }) => {
-    switch (level) {
-      case undefined:
-      case "log":
-        await logClient.send({
-          username: name,
-          content: message,
-        });
-        return;
-      case "info":
-        return;
-      case "error":
-        await logClient.send({
-          username: name,
-          content: "Something went wrong",
-        });
-        return;
-      default:
-        const _: never = level;
-        return;
-    }
   };
 };
